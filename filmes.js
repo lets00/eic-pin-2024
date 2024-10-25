2aa56d58a06a0739eb96cf4325473bbbdc376788
lets00 <eduardovansilva@gmail.com>
2024-10-24 21:41:34 -0300

    (feat) adicionando filmes.js isoladamente para facilitar os alunos obterem o arquivo

---
+++ filmes.js
@@ -0,0 +1,32 @@
+let filmes = [
+  {
+    id: 0,
+    nome: "Tropa de Elite 2: O Inimigo Agora é Outro",
+    valor_ingresso: 25.0,
+    ano: 2010,
+  },
+  {
+    id: 1,
+    nome: "Dona Flor e Seus Dois Maridos",
+    valor_ingresso: 22.0,
+    ano: 1976,
+  },
+  {
+    id: 2,
+    nome: "Minha Mãe é uma Peça 3",
+    valor_ingresso: 20.0,
+    ano: 2019,
+  },
+  {
+    id: 3,
+    nome: "Se Eu Fosse Você 2",
+    valor_ingresso: 18.0,
+    ano: 2009,
+  },
+  {
+    id: 4,
+    nome: "De Pernas pro Ar 2",
+    valor_ingresso: 17.0,
+    ano: 2012,
+  },
+]
